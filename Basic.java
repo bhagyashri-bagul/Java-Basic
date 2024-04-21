065529d1fd2bdef985a9e1a285303e9195a9c968
bhagyashri <104320703+bhagyashri-bagul@users.noreply.github.com>
2024-04-21 09:16:16 +0530

    Add files via upload
    Simple Hello Program & Area of circle program

---
+++ Basic.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Basic{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Scanner sc=new Scanner(System.in);
+float s=sc.nextFloat();
+float b=3.14f*s*s;
+System.out.println(b);
+    }
+}
+
